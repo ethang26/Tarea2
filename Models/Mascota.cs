2a7ecf0003820c650030343ecabbd574fa92ea62
ethang26 <ethancusi2005@gmail.com>
2025-08-21 17:27:21 -0500

    Agregado modelo mascota

---
+++ Models/Mascota.cs
@@ -0,0 +1,11 @@
+namespace Tarea2.Models
+{
+    public class Mascota
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Especie { get; set; }
+        public int Edad { get; set; }
+        public string NombreDueno { get; set; }
+    }
+}
